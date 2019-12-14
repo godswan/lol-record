2bfc3cec8865377346ae55a67f4068f54f2071f7
DONGSU <dlehdtn0401@naver.com>
2019-12-14 13:27:51 +0900

    lt2

---
+++ src/test/java/com/lol/LolRecordApplicationTests2.java
@@ -0,0 +1,13 @@
+package com.lol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
